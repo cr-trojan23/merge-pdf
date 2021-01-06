3e10529f3d999f52541a4cd2296af0e5bdd47ddf
cr-trojan23 <pandusrinu1604@gmail.com>
2021-01-06 16:22:19 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Merge and compress your PDFs into a single PDF
+<br>
+Usage : main.py -f file1.pdf file2.pdf file3.pdf ...
+<br>
+use `pip install requirements.txt` to install all the dependencies.
---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+from PyPDF2 import PdfFileMerger, merger
+
+parser = argparse.ArgumentParser(description= "Merge and compress your PDFs into a single PDF")
+parser.add_argument("--file", "-f", action="store", type=str, nargs="*", help="Location of your PDF files, in the order of their placement")
+args = parser.parse_args()
+merger = PdfFileMerger()
+
+for pdf in args.file:
+    merger.append(pdf)
+merger.write("merged_file.pdf")
+merger.close()
