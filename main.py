887771397dfcd9c8ec856a730ec40312f9a7385e
cr-trojan23 <pandusrinu1604@gmail.com>
2021-01-06 17:44:39 +0530

    Resolved TypeError

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import argparse
 from PyPDF2 import PdfFileMerger, merger
 
+
 parser = argparse.ArgumentParser(description= "Merge and compress your PDFs into a single PDF")
-parser.add_argument("--file", "-f", action="store", type=str, nargs="*", help="Location of your PDF files, in the order of their placement")
+parser.add_argument("--file", "-f", action="store", type=str, nargs="*", help="Location of your PDF files, in the order of their placement", required=True)
 args = parser.parse_args()
 merger = PdfFileMerger()
 
