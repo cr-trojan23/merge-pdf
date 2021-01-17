653f614ed1eb001ddb03ec1bd80e0e51c370b946
cr-trojan23 <pandusrinu1604@gmail.com>
2021-01-17 16:54:25 +0530

    Resolves [#2](https://github.com/cr-trojan23/merge-pdf/issues/1), Enabled user-defined output filename

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from PyPDF2 import PdfFileMerger, merger
 
 parser = argparse.ArgumentParser(description= "Merge and compress your PDFs into a single PDF")
 parser.add_argument("--file", "-f", action="store", type=str, nargs="*", help="Location of your PDF files, in the order of their placement", required=True)
+parser.add_argument("--output", "-o", type=str, help="Name of output file", required=True)
 args = parser.parse_args()
 merger = PdfFileMerger()
 
 for pdf in args.file:
     merger.append(pdf)
-merger.write("merged_file.pdf")
+merger.write(args.output)
 merger.close()
